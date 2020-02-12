0f177eb22eac34014bcc2ad8799ace20834e3715
Luo Hanbing <734778368@qq.com>
2020-02-12 10:02:46 +0800

    Create climbStairs.go

---
+++ dynamic_planning/climbStairs/golang/climbStairs.go
@@ -0,0 +1,45 @@
+// 斐波那契递归
+// func climbStairs(n int) int {
+//     if n < 2{
+//         return 1
+//     }
+//     return climbStairs(n-1)+climbStairs(n-2)
+// }
+
+// 斐波那契非递归
+// func climbStairs(n int) int {
+//     if n < 2{
+//         return 1
+//     } 
+//     if n==2{
+//         return 2
+//     }
+
+//     i,j:= 1,2
+//     ret :=0
+//     for k:= 3;k<=n;k++{
+//         ret = i+j
+//         i=j
+//         j=ret
+//     }
+
+//     return ret
+// }
+
+// 动态规划
+func climbStairs(n int) int {
+    if n < 2{
+        return 1
+    } 
+    if n==2{
+        return 2
+    }
+
+    ret := make([]int,0)
+    ret = append(ret, []int{0,1,2}...) //0占位
+    for k:= 3;k<=n;k++{
+        ret = append(ret, ret[k-1]+ret[k-2])
+    }
+
+    return ret[n]
+}
