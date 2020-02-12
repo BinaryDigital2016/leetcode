09644ab49c99d0af221c815a85adcd83e02ac943
Luo Hanbing <734778368@qq.com>
2020-02-12 11:47:44 +0800

    Create deleteDuplicates.go

---
+++ list/deleteDuplicates/golang/deleteDuplicates.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil{
+        return head
+    }
+    cur := head.Next
+    pre := head
+    for cur != nil{
+        if cur.Val == pre.Val{
+            pre.Next = cur.Next
+            cur = cur.Next
+            continue
+        }
+        pre = cur
+        cur = cur.Next
+    }
+
+    return head
+}
