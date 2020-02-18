d7c4c9986a3f740846306919d44721ab8e455111
Luo Hanbing <734778368@qq.com>
2020-02-18 15:07:55 +0800

    Create hasCycle.go

---
+++ list/hasCycle/golang/hasCycle.go
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+// // map+遍历
+// func hasCycle(head *ListNode) bool {
+//     m := make(map[*ListNode]struct{})
+//     q := head
+//     for q != nil {
+//         if _,ok:=m[q];ok{
+//             return true
+//         }
+//         m[q]=struct{}{}
+//         q = q.Next
+//     }
+//     return false
+// }
+
+ // 快慢指针
+func hasCycle(head *ListNode) bool {
+    if head == nil || head.Next == nil{
+        return false
+    }
+    s := head
+    q := head.Next
+    for q != nil && q.Next != nil{
+        if s == q{
+            return true
+        }
+        s = s.Next
+        q = q.Next.Next
+    }
+    return false
+}
