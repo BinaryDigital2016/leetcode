ebaa5e2300345b370bf1e0af595f7b2f8c7ae16c
Luo Hanbing <734778368@qq.com>
2020-02-24 18:50:21 +0800

    Create reverseList.go

---
+++ list/reverseList/golang/reverseList.go
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+ // 为啥超时 一脸懵逼
+// func reverseList(head *ListNode) *ListNode {
+// 	if head == nil || head.Next == nil {
+// 		return head
+// 	}
+// 	t := &ListNode{
+// 		Val:-1,
+// 		Next:head,
+// 	}
+// 	cur := head.Next
+// 	for cur != nil{
+// 		n := cur.Next
+// 		cur.Next = t.Next
+// 		t.Next = cur
+// 		cur = n
+// 	}
+
+// 	return t.Next
+// }
+
+// func reverseList(head *ListNode) *ListNode {
+//     var pre *ListNode
+// 	cur := head
+// 	for cur != nil{
+// 		n := cur.Next
+// 		cur.Next = pre
+// 		pre = cur
+// 		cur = n
+// 	}
+
+// 	return pre
+// }
+
+// 递归版
+func reverseList(head *ListNode) *ListNode {
+    if head==nil || head.Next==nil {
+        return head
+    }
+    cur := reverseList(head.Next) //最后一个节点
+    head.Next.Next = head //下一个节点指向自己，形成环
+    head.Next = nil //断掉环
+
+
+	return cur
+}
