f26e386da45e8d1d719bb17edd032c3138a7a6fb
Luo Hanbing <734778368@qq.com>
2020-02-29 11:03:45 +0800

    Create moveZero.go

---
+++ others/moveZero/golang/moveZero.go
@@ -0,0 +1,9 @@
+func moveZeroes(nums []int)  {
+    flag := 0
+    for i:=0;i<len(nums);i++ {
+        if nums[i] != 0{
+            nums[i],nums[flag] = nums[flag], nums[i]
+            flag++
+        }
+    }
+}
