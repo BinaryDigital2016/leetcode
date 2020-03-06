883a993ecb6d7e04f6f242e8659d8fd164d5aa2c
luohanbing <luohanbing@bytedance.com>
2020-03-06 18:51:03 +0800

    feat:add wigglesort'
    Change-Id: I5900fa5afe5567661ad21c70d0d81fdace422763

---
+++ sort/wiggleSort/golang/wiggleSort.go
@@ -0,0 +1,28 @@
+package sort
+
+import "sort"
+
+// 排序
+func wiggleSort(nums []int) {
+	newNums := sort.IntSlice(nums)
+	sort.Sort(newNums)
+	n := len(newNums)
+	n1 := n / 2
+	if n%2 == 1 {
+		n1++
+	}
+	pre := make([]int, n1)
+	last := make([]int, n-n1)
+	copy(last, newNums[n1:])
+	copy(pre, newNums[:n1])
+	j, k := n1-1, n-n1-1
+	for i := 0; i < n; i++ {
+		if i%2 == 0 {
+			newNums[i] = pre[j]
+			j--
+		} else {
+			newNums[i] = last[k]
+			k--
+		}
+	}
+}
