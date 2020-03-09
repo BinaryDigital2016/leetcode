570a7102276e1efdd2e0bb224bdaa448ffca9d06
luohanbing <luohanbing@bytedance.com>
2020-03-09 23:28:45 +0800

    feat:增加2个解答
    Change-Id: I44f7a6028deacc036cf3db21f90b507e7408d2dc

---
+++ sort/kClosest/golang/sort.go
@@ -0,0 +1,42 @@
+package sort
+
+// 本质为求k小元素
+
+//排序取前k个,略
+
+//快排变形
+func kClosest(points [][]int, K int) [][]int {
+	partition(points, 0, len(points)-1, K)
+	return points[0:K]
+}
+
+func partition(points [][]int, le, ri, K int) {
+	if le >= ri {
+		return
+	}
+	p := points[le]
+	pivot := dist(p)
+	l, r := le, ri
+	for l < r {
+		for l < r && dist(points[r]) >= pivot {
+			r--
+		}
+		points[l] = points[r]
+		for l < r && dist(points[l]) <= pivot {
+			l++
+		}
+		points[r] = points[l]
+	}
+	points[l] = p
+
+	//l==r
+	if K < l-le+1 { //K位于左半部分
+		partition(points, le, l, K)
+	} else {
+		partition(points, l+1, ri, K-l+le-1)
+	}
+}
+
+func dist(s []int) int {
+	return s[0]*s[0] + s[1]*s[1]
+}
---
+++ sort/pancakeSort/golang/sort.go
@@ -0,0 +1,51 @@
+package sort
+
+/*
+每次都把当前区间最大的数移动到当前区间的最后面
+假设当前区间为[0, i]，该区间内最大的数的下标为j
+那么我们可以通过两次翻转达到目的：
+1，j + 1，将该数移动到区间头部
+2，i + 1，将该数移动到区间尾部
+
+作者：da-li-wang
+链接：https://leetcode-cn.com/problems/pancake-sorting/solution/c-jian-dan-shu-xue-fa-by-da-li-wang/
+*/
+func pancakeSort(A []int) []int {
+	ret := make([]int, 0, len(A))
+	for i := len(A) - 1; i > 0; i-- {
+		j := maxIndex(A[:i+1])
+		if j > 0 {
+			reverse(A[:j+1])
+			ret = append(ret, j+1)
+		}
+		reverse(A[:i+1])
+		ret = append(ret, i+1)
+	}
+	return ret
+}
+
+func maxIndex(s []int) int {
+	if len(s) == 0 {
+		return 0
+	}
+	max := 0
+	for k, v := range s {
+		if v > s[max] {
+			max = k
+		}
+	}
+	return max
+}
+
+func reverse(s []int) {
+	if len(s) == 0 {
+		return
+	}
+
+	i, j := 0, len(s)-1
+	for i < j {
+		s[i], s[j] = s[j], s[i]
+		i++
+		j--
+	}
+}
